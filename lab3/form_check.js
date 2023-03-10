e02bd6c9f1690a2985d9323f2d5f47101fd297b1
pwput <45051480+pwput@users.noreply.github.com>
2023-03-10 16:01:28 +0100

    lab3 done

--- lab3/form_check.js
+++ lab3/form_check.js
@@ -3,7 +3,7 @@ function isWhiteSpaceOrEmpty(str) {
 }
 
 function checkEmail(str) {
-    let email = /^[a-zA-Z_0-9\.]+@[a-zA-Z_0-9\.]+\.[a-zA-Z][a-zA-Z]+$/;
+    let email = /^[a-zA-Z_0-9.]+@[a-zA-Z_0-9.]+\.[a-zA-Z][a-zA-Z]+$/;
     return !email.test(str);
 }
 
@@ -19,6 +19,58 @@ function checkStringAndFocus(obj, msg, fun) {
         return true;
     }
 }
+
+function showElement(e) {
+    document.getElementById(e).style.visibility = 'visible';
+}
+function hideElement(e) {
+    document.getElementById(e).style.visibility = 'hidden';
+}
+
+
+function alterRows(i, e) {
+    if (e) {
+        if (i % 2 === 1) {
+            e.setAttribute("style", "background-color: Aqua;");
+        }
+        e = e.nextSibling;
+        while (e && e.nodeType !== 1) {
+            e = e.nextSibling;
+        }
+        alterRows(++i, e);
+    }
+}
+alterRows(1, document.querySelector("tr"));
+
+
+function nextNode(e) {
+    while (e && e.nodeType != 1) {
+        e = e.nextSibling;
+    }
+    return e;
+}
+function prevNode(e) {
+    while (e && e.nodeType != 1) {
+        e = e.previousSibling;
+    }
+    return e;
+}
+function swapRows(b) {
+    let tab = prevNode(b.previousSibling);
+    let tBody = nextNode(tab.firstChild);
+    let lastNode = prevNode(tBody.lastChild);
+    tBody.removeChild(lastNode);
+    let firstNode = nextNode(tBody.firstChild);
+    tBody.insertBefore(lastNode, firstNode);
+}
+
+function cnt(form, msg, maxSize) {
+    if (form.value.length > maxSize)
+        form.value = form.value.substring(0, maxSize);
+    else
+        msg.innerHTML = maxSize - form.value.length;
+}
+
 function validate(form) {
     checkStringAndFocus(form.elements["f_imie"], "Podaj imię!",isWhiteSpaceOrEmpty);
     checkStringAndFocus(form.elements["f_nazwisko"], "Podaj nazwisko!",isWhiteSpaceOrEmpty);
