82b3ee8d14e52cef4876a343fdcd4b8475b9973a
pwput <45051480+pwput@users.noreply.github.com>
2023-04-12 12:33:44 +0200

    lab 6

--- lab6/cookie.php
+++ lab6/cookie.php
@@ -3,8 +3,19 @@
 <html>
 <head>
     <title>PHP</title>
-    <meta charset='UTF-8' />
+    <meta charset='UTF-8'/>
 </head>
 <body>
+<a href="index.php">wstecz</a>
+<br>
+<?php
+require_once("funkcje.php");
+if (isset($_GET["time"])) {
+    $time = $_GET["time"];
+    setcookie("time", $time , time() + (10), "/");
+    echo "ustawiono cookie time na: " . $_GET["time"];
+}
+echo "Cookie";
+?>
 </body>
 </html>
--- lab6/index.php
+++ lab6/index.php
@@ -10,16 +10,34 @@
 <?php
 echo "<h1>Nasz system</h1>";
 
+
+if (isset($_COOKIE["time"])) {
+    echo "Zapisane cookie: " . $_COOKIE["time"];
+}
+
+
 if (isset($_POST["wyloguj"]) && $_SESSION["zalogowany"] == 1) {
     $_SESSION["zalogowany"] = 0;
 }
 ?>
 <form action="logowanie.php" method="post">
-    <label for="login">Login:</label><br>
-    <input type="text" id="login" name="login"><br>
-    <label for="password">Hasło:</label><br>
-    <input type="password" id="password" name="password">
-    <input type="submit" formmethod="POST" value="Zaloguj" name="send">
+    <fieldset>
+        <legend>Logowanie</legend>
+        <label for="login">Login:</label>
+        <input type="text" id="login" name="login">
+        <br>
+        <label for="password">Hasło:</label>
+        <input type="password" id="password" name="password">
+        <input type="submit" formmethod="POST" value="Zaloguj" name="send">
+    </fieldset>
+</form>
+<form action="cookie.php" method="get">
+    <fieldset>
+        <legend>Cookie Time</legend>
+        <label for="time">Czas:</label>
+        <input type="number" id="time" name="time">
+        <input type="submit" name=create" value="Utworz Cookie">
+    </fieldset>
 </form>
 </body>
 </html>
